690070f47ca95c98edf8888a59a8796381b09377
Tyler Hellmann <4thellmann@gmail.com>
2022-05-15 19:22:46 -0400

    Initial Commit

---
+++ Characters/CharacterRow.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import Kingfisher
+
+struct CharacterRow: View {
+    
+    var character: MarvelCharacter
+    
+    var body: some View {
+        VStack {
+            HStack {
+                
+            }
+        }
+    }
+}
+
+struct CharacterRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterRow(character: MarvelCharacter.ironMan)
+    }
+}
