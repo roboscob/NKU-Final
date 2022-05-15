e1efe7b1e972b48dc64302371f8006787572c6ad
Tyler Hellmann <4thellmann@gmail.com>
2022-05-15 19:28:18 -0400

    Finished project

--- Characters/CharacterList.swift
+++ Characters/CharacterList.swift
@@ -1,27 +1,26 @@
 import SwiftUI
 
 struct CharacterList: View {
-    
     @State var characters = [MarvelCharacter]()
-    
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(characters, id: \.id){ MarvelCharacter in
+                    CharacterRow(character:MarvelCharacter)
+                }
+                .onAppear{
+                    getCharacters()
+                }
             }
             .navigationTitle("Characters")
         }.navigationViewStyle(StackNavigationViewStyle())
     }
-    
     func getCharacters() {
         let networkManager = NetworkManager()
-        
         networkManager.getCharacters(offset: 0, limit: 100) { container, error in
-            
             guard let container = container, let characters = container.results, error == nil else {
                 return
             }
-            
             DispatchQueue.main.async {
                 self.characters = characters
             }
--- Characters/CharacterRow.swift
+++ Characters/CharacterRow.swift
@@ -2,13 +2,19 @@ import SwiftUI
 import Kingfisher
 
 struct CharacterRow: View {
-    
     var character: MarvelCharacter
-    
     var body: some View {
         VStack {
             HStack {
-                
+                KFImage(character.thumbnail?.getImageURL())
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .cornerRadius(6)
+                if character.name==nil{
+                      Text("Unknown")
+                } else{
+                    Text(character.name!)
+                }
             }
         }
     }
